c32e00fcee7b54c08859ff24b6074a316cf2656c
Bjarne Koll <lynxplay101@gmail.com>
2021-07-21 02:54:05 +0200

    Update paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,24 @@
-import io.papermc.paperweight.util.Constants
+import io.papermc.paperweight.util.constants.*
 
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.8"
+    id("io.papermc.paperweight.patcher") version "1.1.11"
 }
 
 repositories {
     mavenCentral()
     maven("https://papermc.io/repo/repository/maven-public/") {
-        content { onlyForConfigurations(Constants.PAPERCLIP_CONFIG) }
+        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
     }
     maven("https://maven.quiltmc.org/repository/release/") {
-        content { onlyForConfigurations(Constants.REMAPPER_CONFIG) }
+        content { onlyForConfigurations(REMAPPER_CONFIG) }
     }
 }
 
 dependencies {
-    remapper("org.quiltmc:tiny-remapper:0.4.1")
+    remapper("org.quiltmc:tiny-remapper:0.4.3")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.12")
     paperclip("io.papermc:paperclip:2.0.1")
 }
 
@@ -52,6 +53,9 @@ subprojects {
 paperweight {
     serverProject.set(project(":KTP-Server"))
 
+    remapRepo.set("https://maven.quiltmc.org/repository/release/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
     usePaperUpstream(provider { file("current-paper").readText().trim() }) {
         withPaperPatcher {
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
@@ -66,8 +70,4 @@ paperweight {
 tasks.paperclipJar {
     destinationDirectory.set(rootProject.layout.projectDirectory)
     archiveFileName.set("ktp-paperclip.jar")
-}
-
-tasks.withType<io.papermc.paperweight.patcher.tasks.SimpleRebuildGitPatches> {
-    filterPatches.set(false)
 }
