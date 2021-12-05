2dbd8cd26114fcc04e3e2a865c36af5b2b85a633
Bjarne Koll <32834385+lynxplay@users.noreply.github.com>
2021-12-05 02:55:37 +0100

    Add test plugin layout to ease testing api changes (#3)
    To ease testing new additions to the api, this commit adds a test plugin
    that may be used to develop locally. The test plugin will automatically
    be added to any server ran through 'runDev' or the likes if enabled.
    
    To ensure no changes to the test plugin are commited to the repository,
    it has to be excluded from git future tracking through the following
    bash commands:
     - echo "test-plugin" >> .git/info/exclude
     - find test-plugin -type f | xargs git update-index --assume-unchanged
    And uncommenting the include line in 'test-plugin.settings.gradle.kt'.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,10 @@ pluginManagement {
 rootProject.name = "ktp"
 
 include("KTP-API", "KTP-Server")
+
+val testPlugin = file("test-plugin.settings.gradle.kts")
+if (testPlugin.exists()) {
+    apply(from = testPlugin)
+} else {
+    testPlugin.writeText("// Uncomment to enable the test plugin module\n//include(\":test-plugin\")\n")
+}
