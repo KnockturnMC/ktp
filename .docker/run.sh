4be995a689b25532a5d779c627f2accbf30592ac
Bjarne Koll <lynxplay101@gmail.com>
2023-06-24 18:27:43 +0200

    Create dockerfile for ktp

---
+++ .docker/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+exec java -Xms"$SERVER_MEMORY" -Xmx"$SERVER_MEMORY" --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 \
+-XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 \
+-XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 \
+-XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -XX:G1NewSizePercent=30 \
+-XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar /bin/server.jar --nogui
