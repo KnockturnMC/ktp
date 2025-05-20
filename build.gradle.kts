faf27bf7ea6d4e658dff1a67129a5441c2fa5543
Bjarne Koll <git@lynxplay.dev>
2025-05-20 13:45:53 +0200

    Configure publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,12 +74,10 @@ subprojects {
 
     extensions.configure<PublishingExtension> {
         repositories {
-            /*
-            maven("https://repo.papermc.io/repository/maven-snapshots/") {
-                name = "paperSnapshots"
+            maven("https://repo.knockturnmc.com/repository/knockturn-public-snapshot/") {
+                name = "knockturnPublic"
                 credentials(PasswordCredentials::class)
             }
-             */
         }
     }
 }
