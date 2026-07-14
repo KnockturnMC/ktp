d5181fa1d8973fc1335d9916eece3161723e4107
Bjarne Koll <git@lynxplay.dev>
2026-07-14 22:27:24 +0100

    Update build infra

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,6 @@ paperweight {
     }
 }
 
-val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
-
 subprojects {
     apply(plugin = "java-library")
     apply(plugin = "maven-publish")
@@ -39,37 +37,34 @@ subprojects {
             languageVersion = JavaLanguageVersion.of(25)
         }
     }
+}
 
-    repositories {
-        mavenCentral()
-        maven(paperMavenPublicUrl)
-    }
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
 
-    tasks.withType<AbstractArchiveTask>().configureEach {
-        isPreserveFileTimestamps = false
-        isReproducibleFileOrder = true
-    }
-
-    tasks.withType<JavaCompile> {
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
         options.encoding = Charsets.UTF_8.name()
         options.release = 25
         options.isFork = true
+        options.compilerArgs.addAll(listOf("-Xlint:-deprecation", "-Xlint:-removal"))
     }
-
-    tasks.withType<Javadoc> {
+    tasks.withType<Javadoc>().configureEach {
         options.encoding = Charsets.UTF_8.name()
     }
-
-    tasks.withType<ProcessResources> {
+    tasks.withType<ProcessResources>().configureEach {
         filteringCharset = Charsets.UTF_8.name()
     }
-
-    tasks.withType<Test> {
+    tasks.withType<Test>().configureEach {
         testLogging {
             showStackTraces = true
             exceptionFormat = TestExceptionFormat.FULL
             events(TestLogEvent.STANDARD_OUT)
         }
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
     }
 
     extensions.configure<PublishingExtension> {
@@ -82,5 +77,19 @@ subprojects {
         publications.create<MavenPublication>("local") {
             from(components["java"])
         }
+    }
+}
+
+tasks.register("printMinecraftVersion") {
+    val mcVersion = providers.gradleProperty("mcVersion")
+    doLast {
+        println(mcVersion.get().trim())
+    }
+}
+
+tasks.register("printPaperVersion") {
+    val paperVersion = provider { project.version }
+    doLast {
+        println(paperVersion.get())
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,47 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "ktp"
 
-include("ktp-api", "ktp-server")
+for (name in listOf("ktp-api", "ktp-server")) {
+    include(name)
+    file(name).mkdirs()
+}
 
-val testPlugin = file("test-plugin.settings.gradle.kts")
-if (testPlugin.exists()) {
-    apply(from = testPlugin)
-} else {
-    testPlugin.writeText("// Uncomment to enable the test plugin module\n//include(\":test-plugin\")\n")
+optionalInclude("test-plugin")
+
+fun optionalInclude(name: String, op: (ProjectDescriptor.() -> Unit)? = null) {
+    val settingsFile = file("$name.settings.gradle.kts")
+    if (settingsFile.exists()) {
+        apply(from = settingsFile)
+        findProject(":$name")?.let { op?.invoke(it) }
+    } else {
+        settingsFile.writeText(
+            """
+            // Uncomment to enable the '$name' project
+            // include(":$name")
+
+            """.trimIndent()
+        )
+    }
+}
+
+gradle.lifecycle.beforeProject {
+    val mcVersion = providers.gradleProperty("mcVersion").get().trim()
+    val paperVersionChannel = providers.gradleProperty("channel").get().trim()
+    val paperBuildNumber = providers.environmentVariable("BUILD_NUMBER").orNull?.trim()?.toInt()
+    val versionString = if (paperBuildNumber == null) {
+        "$mcVersion.local-SNAPSHOT"
+    } else {
+        "$mcVersion.build.$paperBuildNumber-${paperVersionChannel.lowercase()}"
+    }
+    version = versionString
 }
