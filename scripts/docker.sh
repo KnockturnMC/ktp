89deb8e3927245c2a14636b245abe4b0e9a2f21d
Bjarne Koll <git@lynxplay.dev>
2026-09-25 20:16:33 +0100

    Also push optionally

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,3 +2,8 @@
 MINECRAFT_VERSION="$(./gradlew -q printMinecraftVersion)"
 docker buildx build -f .docker/Dockerfile . --target runner-jbr -t "docker.knockturnmc.com/ktp/ktp-server:$MINECRAFT_VERSION-jbr"
 docker buildx build -f .docker/Dockerfile . --target runner-corretto -t "docker.knockturnmc.com/ktp/ktp-server:$MINECRAFT_VERSION"
+
+if [ "${1:-x}" == "--push" ]; then
+    docker push "docker.knockturnmc.com/ktp/ktp-server:$MINECRAFT_VERSION-jbr"
+    docker push "docker.knockturnmc.com/ktp/ktp-server:$MINECRAFT_VERSION"
+fi
